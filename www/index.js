26f816326ce36cfa9738aef25402e6f186630b60
osawa-koki <koki@osawa.dev>
2023-10-28 19:08:03 +0900

    パフォーマンスの測定。

--- www/index.js
+++ www/index.js
@@ -10,6 +10,48 @@ const universe = Universe.new();
 const width = universe.width();
 const height = universe.height();
 
+const fps = new class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = 1 / delta * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+      Frames per Second: latest = ${Math.round(fps)}
+      avg of last 100 = ${Math.round(mean)}
+      min of last 100 = ${Math.round(min)}
+      max of last 100 = ${Math.round(max)}
+    `.trim();
+  }
+};
+
 const playPauseButton = document.getElementById("play-pause");
 
 // Give the canvas room for all of our cells and a 1px border
@@ -72,6 +114,7 @@ const getIndex = (row, column) => {
 let animationId = null;
 const renderLoop = () => {
   // debugger;
+  fps.render(); //new
   universe.tick();
 
   drawGrid();
